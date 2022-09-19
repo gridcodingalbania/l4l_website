041fb944a5bb3bd39272df95dbe2d9a3c18a11a5
orjanab <orjana@gridcoding.com>
2022-09-19 16:13:58 +0200

    add footer -done

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
-<?php get_header(); ?>
+<style>
+<?php include 'style.css'; ?>
+</style>
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Arimo" />
+<?php  get_header(); ?>
 index
 <?php get_footer(); ?>
