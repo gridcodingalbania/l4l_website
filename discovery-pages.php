d776159b6525bd9a8ba8ff4939c7be174bfe0f9d
Greit <greitopingari@gmail.com>
2022-09-26 16:53:13 +0200

    menu section function

--- discovery-pages.php
+++ discovery-pages.php
@@ -18,26 +18,26 @@
     <div class="w-full">
         <ul class="flex items-center justify-between w-full px-4 py-4 border-b-[1px] ">
             <li class="mr-6">
-                <a class="text-blue-500 hover:text-red-600" href="#">Cream to Powder</a>
+                <a class="hover:text-red-600" href="#" onClick="toggleDiv('cream')">Cream to Powder</a>
             </li>
             <li class="mr-6">
-                <a class="text-blue-500 hover:text-red-600" href="#">Velvet</a>
+                <a class=" hover:text-red-600" href="#" onClick="toggleDiv('velvet')">Velvet</a>
             </li>
             <li class="mr-6">
-                <a class="text-blue-500 hover:text-red-600" href="#">Metallic</a>
+                <a class="hover:text-red-600" href="#" onClick="toggleDiv('metallic')">Metallic</a>
             </li>
             <li class="mr-6">
-                <a class="text-blue-500 hover:text-red-600" href="#">Accessories</a>
+                <a class=" hover:text-red-600" href="#" onClick="toggleDiv('accessories')">Accessories</a>
             </li>
             <li class="mr-6">
-                <a class="text-blue-500 hover:text-red-600" href="#">Roar iD</a>
+                <a class="hover:text-red-600" href="#" onClick="toggleDiv('roarid')">Roar iD</a>
             </li>
             
         </ul>
     </div>
 
     <!-- Cream to Powder Section -->
-    <div class="hidden" id="cream">
+    <div class="pages" id="cream">
         <!-- Topic Section -->
         <div class="w-full topic-section text-center py-4">
             <h1 class="text-[48px] text-red-600">Topic 8</h1>
@@ -85,8 +85,11 @@
 
     </div>
 
+
+
+
     <!-- Velvet Section -->
-    <div class="" id="velvet">
+    <div class="pages" id="velvet" style="display:none;">
         <!-- Topic Section -->
         <div class="w-full topic-section text-center py-4">
             <h1 class="text-[48px] text-red-600">Topic 8</h1>
@@ -118,9 +121,12 @@
             <img src="<?php echo 'cosmeticimage.png' ?>">
             </div>
             <div class="w-[70%]">
-                <div>
+                <div class="flex flex-row content-center justify-center space-x-10">
                     <p>Usa filtri Beauty Wallet</p>
-                    <button></button>
+                    <label for="default-toggle" class="inline-flex relative items-center cursor-pointer">
+                        <input type="checkbox" value="" id="default-toggle" class="sr-only peer">
+                        <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    </label>
                 </div>
                 
             </div>
@@ -146,5 +152,31 @@
 
 
     </div>
+
+    <!-- Metallic Section -->
+    <div class="pages" id="metallic" style="display:none;"></div>
+
+
+    <!-- Accessories Section -->
+    <div class="pages" id="accessories" style="display:none;"></div>
+
+    <!-- Roarid Section -->
+    <div class="pages" id="roarid" style="display:none;"></div>
+
+    
+    <script>
+        function toggleDiv(target){
+                    hideAllPages();
+                    document.getElementById(target).style.display = 'block';
+                    return false;
+        }
+
+        function hideAllPages(){
+                    var pages = document.getElementsByClassName("pages");
+                    for(var i = 0; i < pages.length; i++){
+                        pages[i].style.display = 'none';
+                    }
+}
+    </script>
 </body>
 <?php get_footer(); ?>
