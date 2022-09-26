c5cce33ea00f4eaba0fd8fe4cbcc5e9de96ee091
Greit <greitopingari@gmail.com>
2022-09-26 13:49:19 +0200

    discovery pages

---
+++ discovery-pages.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Template Name: Discovery-Pages
+ */
+?>
+<?php get_header(); ?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <!-- Discovery Pages Navigation -->
+    <div class="w-full">
+        <ul class="flex items-center justify-between w-full px-4 py-4 border-b-[1px] ">
+            <li class="mr-6">
+                <a class="text-blue-500 hover:text-red-600" href="#">Cream to Powder</a>
+            </li>
+            <li class="mr-6">
+                <a class="text-blue-500 hover:text-red-600" href="#">Velvet</a>
+            </li>
+            <li class="mr-6">
+                <a class="text-blue-500 hover:text-red-600" href="#">Metallic</a>
+            </li>
+            <li class="mr-6">
+                <a class="text-blue-500 hover:text-red-600" href="#">Accessories</a>
+            </li>
+            <li class="mr-6">
+                <a class="text-blue-500 hover:text-red-600" href="#">Roar iD</a>
+            </li>
+            
+        </ul>
+    </div>
+
+    <!-- Cream to Powder Section -->
+    <div class="hidden" id="cream">
+        <!-- Topic Section -->
+        <div class="w-full topic-section text-center py-4">
+            <h1 class="text-[48px] text-red-600">Topic 8</h1>
+            <p class="text-[24px]">Discovery product overview</p>
+        </div>
+        <!-- Color Section -->
+        <div class="w-full h-[560px] " style="background-color:#F8BDB1;"></div>
+
+        <!-- ---------- -->
+
+        <!-- Topic Section -->
+        <div class="w-full topic-section text-center py-20">
+            <h1 class="text-[48px] text-red-600">Topic 9</h1>
+        </div>
+        <!-- Color Section -->
+        <div class="w-full h-[560px] " style="background-color:#EE7067;"></div>
+
+        <!-- ---------- -->
+        
+        <!-- Banner Section -->
+        <div class="w-full topic-section text-center py-20">
+            <h1 class="text-[48px] text-red-600">Banner 5</h1>
+        </div>
+        <!-- Color Section -->
+        <div class="w-full h-[560px] " style="background-color:#1E1E1E;"></div>
+
+        <!-- ---------- -->
+
+        <!-- Topic Section -->
+        <div class="w-full topic-section text-center py-20">
+            <h1 class="text-[48px] text-red-600">Topic 10</h1>
+        </div>
+        <!-- Color Section -->
+        <div class="w-full h-[560px] " style="background-color:#668CA1;"></div>
+
+        <!-- ---------- -->
+        
+        <!-- Banner Section -->
+        <div class="w-full topic-section text-center py-20">
+            <h1 class="text-[48px] text-red-600">Banner 6</h1>
+        </div>
+        <!-- Color Section -->
+        <div class="w-full h-[560px] " style="background-color:#1E1E1E;"></div>
+
+
+    </div>
+
+    <!-- Velvet Section -->
+    <div class="" id="velvet">
+        <!-- Topic Section -->
+        <div class="w-full topic-section text-center py-4">
+            <h1 class="text-[48px] text-red-600">Topic 8</h1>
+            <p class="text-[24px]">Discovery product overview</p>
+        </div>
+        <!-- Color Section -->
+        <div class="w-full h-[560px] " style="background-color:#F8BDB1;"></div>
+
+        <!-- ---------- -->
+
+        <!-- Topic Section -->
+        <div class="w-full topic-section text-center py-20">
+            <h1 class="text-[48px] text-red-600">Banner 8</h1>
+        </div>
+        <!-- Color Section -->
+        <div class="w-full h-[560px] " style="background-color:#1E1E1E;"></div>
+
+        <!-- ---------- -->
+        
+        <!-- Banner Section -->
+        <div class="w-full topic-section text-center py-20">
+            <h1 class="text-[48px] text-red-600">Support to select SKU color</h1>
+            <p class="text-[24px]">Shades Overview</p>
+            <p class=" w-[50%] m-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo eu arcu, ultricies lacus gravida ornare nisi in. Vitae adipiscing in enim, habitasse sed ultricies id ac. </p>
+        </div>
+        <!-- Select Color Section -->
+        <div class="w-full h-[560px] flex flex-row ">
+            <div class="image-section w-[30%]">
+            <img src="<?php echo 'cosmeticimage.png' ?>">
+            </div>
+            <div class="w-[70%]">
+                <div>
+                    <p>Usa filtri Beauty Wallet</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- ---------- -->
+
+        <!-- Topic Section -->
+        <div class="w-full topic-section text-center py-20">
+            <h1 class="text-[48px] text-red-600">Topic 5</h1>
+        </div>
+        <!-- Color Section -->
+        <div class="w-full h-[560px] " style="background-color:#F3988B;"></div>
+
+        <!-- ---------- -->
+        
+        <!-- Banner Section -->
+        <div class="w-full topic-section text-center py-20">
+            <h1 class="text-[48px] text-red-600">Banner 11</h1>
+        </div>
+        <!-- Color Section -->
+        <div class="w-full h-[560px] " style="background-color:#1E1E1E;"></div>
+
+
+    </div>
+</body>
+<?php get_footer(); ?>
