07fd06d9c7503f275dadff628c070494ca6eabdb
Greit <greitopingari@gmail.com>
2022-09-27 15:30:46 +0200

    button roarid

--- pages/store.php
+++ pages/store.php
@@ -5,7 +5,7 @@
 ?>
 <?php get_header(); ?>
 <div class="store w-full bg-black py-20">
-    <h1 class="text-[48px] pl-8 py-10" style="color:#E84246;">Store</h1>
+    <h1 class="text-[48px] pl-8 pb-10" style="color:#E84246;">Store</h1>
     <div class="px-10">
         <?php 
         $args = [
@@ -92,21 +92,21 @@
     </div>
 </div>
 <!-- RoarID Section -->
-<div class="roarid px-10 py-20 " style="background-color:linear-gradient(168.58deg, #D2CDB1 2.76%, #9D9982 97.7%);">
+<div class="roarid px-10 py-20 " style="background-color: linear-gradient(168.58deg, #D2CDB1 2.76%, #9D9982 97.7%);">
     <h1 class="text-black text-[48px]">RoarID</h1>
     <div class="flex mb-2">
         <div class="image-section w-1/2">
             <img src="<?php echo get_image('MIX&Match.png');?>">
         </div>
-        <div class="content-section w-1/2">
-            <p>key message</p>
-            <div class="buttons-section">
+        <div class="content-section w-1/2 m-auto text-center">
+            <p class="text-center text-[28px]" style="color:#707070;">key message</p>
+            <div class="buttons-section pt-[150px]">
                 <!-- View More Button -->
-                <button class="bg-transparent text-black font-semibold  py-2 px-4 border border-black rounded">
+                <button class="bg-transparent text-black font-semibold  py-2 px-4 border border-black rounded text-center">
                     View More
                 </button>
                 <!-- Download Button -->
-                <div class="flex mt-3 w-48 h-14 bg-black text-white rounded-xl items-center justify-center">
+                <div class="flex m-auto mt-3 w-48 h-14 bg-black text-white rounded-xl items-center justify-center">
                     <div class="mr-3">
                         <svg viewBox="0 0 384 512" width="30" >
                             <path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"/>
